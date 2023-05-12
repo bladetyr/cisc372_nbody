f4ad6d87a321b4956487e9347d4974a8bd00407e
bladetyr <bladetyr@udel.edu>
2023-05-12 12:40:14 -0400

    we testing

--- compute.cu
+++ compute.cu
@@ -56,9 +56,9 @@ __global__ void sumMatrix(vector3 d_hVel, vector3 d_hPos, vector3 accels){
 //Returns: None
 //Side Effect: Modifies the hPos and hVel arrays with the new positions and accelerations after 1 INTERVAL
 void compute(){
-	vector3* dValues;
-	vector3* dAccels;
-	double* d_mass;
+	vector3 dValues;
+	vector3 dAccels;
+	double d_mass;
 
 	//cuda versions of values and accels
 	cudaMalloc((void**)&dValues, sizeof(float)*NUMENTITIES*NUMENTITIES);
@@ -73,6 +73,7 @@ void compute(){
 
 	cudaMemcpy(d_hVel, hVel, sizeof(double)*NUMENTITIES, cudaMemcpyHostToDevice);
 	cudaMemcpy(d_hPos, hPos, sizeof(double)*NUMENTITIES, cudaMemcpyHostToDevice);
+	cudaMemcpy(d_mass, mass, sizeof(double), cudaMemcpyHosttoDevice);
 
 	accelMatrix<<<1,1>>>(&dValues, dAccels, &d_hVel, &d_hPos, &d_mass);
 	cudaDeviceSynchronize();
